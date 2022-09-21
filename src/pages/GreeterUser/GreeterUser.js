90a08161e922147634392212a26af73d41c520e5
Pavel545 <110118389+Pavel545@users.noreply.github.com>
2022-09-21 13:20:15 +0400

    adding and processing prettier

---
+++ src/pages/GreeterUser/GreeterUser.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Authorization } from '../../components/Authorization/Authorization'
+
+function GreeterUser() {
+    const [user, setUser] = useState(null)
+    const handleLogin = () => setUser({ login: 'taradam' })
+    const handleLogout = () => setUser(null)
+
+    return (
+        <div className="App">
+            <div className="App-layout">
+                <Authorization
+                    user={user}
+                    onAuthButtonClick={user ? handleLogout : handleLogin}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default GreeterUser
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import { NotFound } from "./pages/not-found";
-import { ProtectedRoute } from "./components/Protectet-eout";
+import { NotFound } from "./pages/not-found/NotFound";
+import { ProtectedRoute } from "./components/Protectet-eout/Protectet-eout";
 import Main from "./components/Main/Main";
 import Bar from "./components/Bar/Bar";
-import GreeterUser from "./GreeterUser ";
+import GreeterUser from "./pages/GreeterUser/GreeterUser";
+
 export const AppRoutes = ({user}) => {
   return (
     <Routes>
       <Route path="/" element={<GreeterUser/>} />
-
+      
       <Route element={<ProtectedRoute isAllowed={Boolean(user)} />}>
         <Route path="/content" element={<div><Main/><Bar/></div>} />
 
